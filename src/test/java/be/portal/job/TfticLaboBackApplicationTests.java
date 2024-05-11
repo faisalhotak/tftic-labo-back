a0c7f2cc80c15791a3c4a24067cc9ff8138272de
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-05-11 16:44:53 +0200

    init: spring boot project initialization

---
+++ src/test/java/be/portal/job/TfticLaboBackApplicationTests.java
@@ -0,0 +1,13 @@
+package be.portal.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfticLaboBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
