3151c7d378e984013c21579489c4d31964d90886
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-05-13 19:10:05 +0200

    init: switched project structure to multi modules structure

--- pl/src/main/java/be/portal/job/JobPortalApplication.java
+++ pl/src/main/java/be/portal/job/JobPortalApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TfticLaboBackApplication {
-
+public class JobPortalApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TfticLaboBackApplication.class, args);
+        SpringApplication.run(JobPortalApplication.class, args);
     }
-
 }
