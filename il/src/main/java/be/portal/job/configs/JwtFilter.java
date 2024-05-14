30453d683919350cffaaf502a3384f7368bc522b
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-05-14 19:36:36 +0200

    feat: added Jwt config with Security config and externalized the properties into an .env file

---
+++ il/src/main/java/be/portal/job/configs/JwtFilter.java
@@ -0,0 +1,53 @@
+package be.portal.job.configs;
+
+import be.portal.job.services.UserService;
+import be.portal.job.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (!authHeader.isBlank() && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            if (!token.isBlank() && jwtUtils.validateToken(token)) {
+                    String username = jwtUtils.getUsername(token);
+                    UserDetails userDetails = userService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken upt = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities()
+                    );
+
+                    SecurityContextHolder.getContext().setAuthentication(upt);
+                }
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
