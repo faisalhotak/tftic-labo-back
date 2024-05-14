22af74a878b67581953005ad31140cbb50ad3b88
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-05-14 19:35:22 +0200

    feat: added User repo, service and service implementation

---
+++ bll/src/main/java/be/portal/job/services/UserService.java
@@ -0,0 +1,24 @@
+package be.portal.job.services;
+
+import be.portal.job.entities.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+    /**
+     * Authenticates a user with the provided email and password.
+     *
+     * @param email The email of the user trying to log in.
+     * @param password The password of the user trying to log in.
+     * @return The authenticated User object if the login is successful.
+     */
+    User login(String email, String password);
+
+    /**
+     * Registers a new user in the system.
+     *
+     * @param user The User object containing the details of the user to be registered.
+     * @return The registered User object if the registration is successful.
+     */
+    User register(User user);
+}
---
+++ dal/src/main/java/be/portal/job/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package be.portal.job.repositories;
+
+import be.portal.job.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(String email);
+}
