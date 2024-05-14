367089903624c9b86855634cb59d94d663c7fd3b
Lopidurs <nathan.sancke@hotmail.fr>
2024-05-14 11:40:08 +0200

    Feat(JobOffer): add JobFunction

---
+++ domain/src/main/java/be/portal/job/entities/JobFunction.java
@@ -0,0 +1,25 @@
+package be.portal.job.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity
+@ToString
+@Table(name = "job_function")
+public class JobFunction extends BaseEntity<Long> {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @OneToMany(mappedBy = "jobFunction")
+    private Set<JobOffer> jobOffers;
+}
