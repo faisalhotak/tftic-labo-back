a46aaeab0e5e945352ccfed258c761ebfd76423b
Lopidurs <nathan.sancke@hotmail.fr>
2024-05-14 11:24:13 +0200

    Feat(JobAdvertiser): add CompanyAdvertiser entity
    Adding of the CompanayAdvertiser with it's enum AdvertiserRole

--- domain/src/main/java/be/portal/job/entities/CompanyAdvertiser.java
+++ domain/src/main/java/be/portal/job/entities/CompanyAdvertiser.java
@@ -1,2 +1,26 @@
-package be.portal.job.entities;public class CompanyAdvertiser {
+package be.portal.job.entities;
+
+import be.portal.job.enums.AdvertiserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Entity
+@ToString
+@Table(name = "Company_Advertiser")
+public class CompanyAdvertiser extends BaseEntity<Long> {
+
+    @Column(nullable = false, name = "advertiser_role")
+    @Enumerated(EnumType.STRING)
+    private AdvertiserRole advertiserRole;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "job_advertiser_id")
+    private JobAdvertiser jobAdvertiser;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "company_id")
+    private Company company;
 }
--- domain/src/main/java/be/portal/job/enums/AdvertiserRole.java
+++ domain/src/main/java/be/portal/job/enums/AdvertiserRole.java
@@ -1,2 +1,7 @@
-package be.portal.job.enums;public enum AdvertiserRole {
+package be.portal.job.enums;
+
+public enum AdvertiserRole {
+    OWNER,
+    EMPLOYEE,
+    PARTNER
 }
