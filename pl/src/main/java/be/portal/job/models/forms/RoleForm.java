f860dead2a304c859f12dac56d785a3919a9d48a
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-05-15 15:55:39 +0200

    feature : fix

--- pl/src/main/java/be/portal/job/models/forms/RoleForm.java
+++ pl/src/main/java/be/portal/job/models/forms/RoleForm.java
@@ -6,10 +6,8 @@ import jakarta.validation.constraints.Size;
 
 public record RoleForm(
         @NotBlank(message = "Le nom du rôle ne peut pas être vide")
-        @Size(max = 256, message = "Le nom du rôle ne peut pas dépasser {max} caractères")
         String name,
 
-        @Size(max = 1028, message = "La description du rôle ne peut pas dépasser {max} caractères")
         String description
 ) {
     public Role toEntity() {
