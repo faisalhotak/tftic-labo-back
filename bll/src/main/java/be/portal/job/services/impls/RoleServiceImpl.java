533e6acb5f5768a63e024b008d47b4024afb643f
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-05-15 11:29:21 +0200

    feature : implements IRoleService in RoleServiceImpl with methods

---
+++ bll/src/main/java/be/portal/job/services/impls/RoleServiceImpl.java
@@ -0,0 +1,60 @@
+package be.portal.job.services.impls;
+
+import be.portal.job.entities.Role;
+import be.portal.job.repositories.RoleRepository;
+import be.portal.job.services.IRoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements IRoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public List<Role> getRoles() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Optional<Role> getRoleByName(String name) {
+        Optional<Role> role = roleRepository.findByName(name);
+        return role.isPresent() ? role : Optional.empty();
+    }
+
+    @Override
+    public Role getRoleById(Long id) {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Role not found with id: " + id));
+    }
+
+    @Override
+    public Role addRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role updateRole(Long id, Role role) {
+        Role existingRole = roleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Role not found with id: " + id));
+
+        existingRole.setName(role.getName());
+        existingRole.setDescription(role.getDescription());
+        existingRole.setUsers(role.getUsers());
+
+        return roleRepository.save(existingRole); // Save the updated role
+    }
+
+    @Override
+    public void deleteRole(Long id) {
+        if (roleRepository.existsById(id)) {
+            roleRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Role not found with id: " + id);
+        }
+    }
+}
