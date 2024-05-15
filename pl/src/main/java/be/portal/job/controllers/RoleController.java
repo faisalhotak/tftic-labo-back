3c7d91ca429d8f76e94f6f160b518aeb6242331c
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-05-15 14:40:27 +0200

    feature : create RoleController

---
+++ pl/src/main/java/be/portal/job/controllers/RoleController.java
@@ -0,0 +1,64 @@
+package be.portal.job.controllers;
+
+import be.portal.job.models.dtos.RoleDTO;
+import be.portal.job.models.forms.RoleForm;
+import be.portal.job.services.IRoleService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/role")
+@CrossOrigin("*")
+public class RoleController {
+
+    public final IRoleService roleService;
+
+    @GetMapping
+    public ResponseEntity<List<RoleDTO>> getRoles() {
+        List<RoleDTO> roles = roleService.getRoles()
+                .stream()
+                .map(RoleDTO::fromEntity)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(roles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleDTO> getRoleById(@PathVariable Long id) {
+        RoleDTO roles = RoleDTO.fromEntity(roleService.getRoleById(id));
+
+        return ResponseEntity.ok(roles);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @PostMapping("/post")
+    public ResponseEntity<RoleDTO> addRole(@Valid @RequestBody RoleForm roleForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        RoleDTO newRole = RoleDTO.fromEntity(roleService.addRole(roleForm.toEntity()));
+
+        return ResponseEntity.ok(newRole);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<RoleDTO> updateRole(@PathVariable Long id, @Valid @RequestBody RoleForm roleForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+        RoleDTO updatedRole = RoleDTO.fromEntity(roleService.updateRole(id, roleForm.toEntity()));
+
+        return ResponseEntity.ok(updatedRole);
+    }
+
+}
