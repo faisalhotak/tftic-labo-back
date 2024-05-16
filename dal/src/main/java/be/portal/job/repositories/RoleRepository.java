cfad11ec9a011332bd02b9ab7c818eb804076e88
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-05-16 11:49:57 +0200

    feature : add Optional

--- dal/src/main/java/be/portal/job/repositories/RoleRepository.java
+++ dal/src/main/java/be/portal/job/repositories/RoleRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     @Query("SELECT r FROM Role r WHERE r.name = :name")
-    Role findByName(@Param("name") String name);
+    Optional<Role> findByName(@Param("name") String name);
 }
