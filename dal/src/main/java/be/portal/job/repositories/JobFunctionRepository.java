ed389fa8876ac55cad9312aa3a0a7e5f82cabd60
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-05-16 10:23:33 +0200

    feature : ADD @QUERY

--- dal/src/main/java/be/portal/job/repositories/JobFunctionRepository.java
+++ dal/src/main/java/be/portal/job/repositories/JobFunctionRepository.java
@@ -2,10 +2,13 @@ package be.portal.job.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import be.portal.job.entities.JobFunction;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JobFunctionRepository extends JpaRepository<JobFunction, Long> {
 
-    JobFunction findByName(String name);
+    @Query("SELECT jf FROM JobFunction jf WHERE jf.name = :name")
+    JobFunction findByName(@Param("name") String name);
 }
