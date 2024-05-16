3040c2bf8e617316d7f43b9d0bd36f415d3e9298
Kevin Joeghmans <k.joeghmans@stag.technofuturtic.education>
2024-05-16 11:38:26 +0200

    feature : add @NotBlank

--- pl/src/main/java/be/portal/job/models/forms/JobFunctionForm.java
+++ pl/src/main/java/be/portal/job/models/forms/JobFunctionForm.java
@@ -4,9 +4,9 @@ import be.portal.job.entities.JobFunction;
 import jakarta.validation.constraints.NotBlank;
 
 public record JobFunctionForm (
-        @NotBlank(message="The role name cannot be empty.")
+        @NotBlank(message="The job function name cannot be empty.")
         String name,
-
+        @NotBlank(message="The job function title cannot be empty.")
         String title
 ) {
     public JobFunction toEntity() {
--- pl/src/main/java/be/portal/job/models/forms/RoleForm.java
+++ pl/src/main/java/be/portal/job/models/forms/RoleForm.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 public record RoleForm (
         @NotBlank(message = "The role name cannot be empty.")
         String name,
-
+        @NotBlank(message = "The role description cannot be empty.")
         String description
 ) {
     public Role toEntity() {
