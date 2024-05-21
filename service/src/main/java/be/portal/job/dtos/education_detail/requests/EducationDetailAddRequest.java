f57d060febe94efef7f3da4df7f4e998f49edf1d
Tomás Mendes <tomashm16@gmail.com>
2024-05-21 12:05:33 +0200

    feat: add education detail request

---
+++ service/src/main/java/be/portal/job/dtos/education_detail/requests/EducationDetailAddRequest.java
@@ -0,0 +1,46 @@
+package be.portal.job.dtos.education_detail.requests;
+
+import be.portal.job.entities.EducationDetail;
+import be.portal.job.entities.JobSeeker;
+import be.portal.job.enums.DegreeType;
+import be.portal.job.enums.Mention;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record EducationDetailAddRequest(
+
+        @NotBlank(message = "Institute name cannot be blank")
+        String instituteName,
+
+        @NotBlank(message = "Major cannot be blank")
+        String major,
+
+        @NotNull(message = "Degree type cannot be null")
+        DegreeType degreeType,
+
+        @NotNull(message = "Mention cannot be null")
+        Mention mention,
+
+        @NotNull(message = "Start date cannot be null")
+        LocalDateTime startDate,
+
+        @NotNull(message = "Completion date cannot be null")
+        LocalDateTime completionDate,
+
+        @NotNull
+        JobSeeker jobSeeker
+) {
+    public EducationDetail toEntity() {
+        return new EducationDetail(
+                instituteName,
+                major,
+                degreeType,
+                mention,
+                startDate,
+                completionDate,
+                jobSeeker
+        );
+    }
+}
