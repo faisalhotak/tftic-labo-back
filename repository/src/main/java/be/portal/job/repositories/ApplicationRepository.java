416e12dda3dd8e0c71e05456d08ace504eb95d94
Kei-gits <kioo-17@hotmail.com>
2024-05-23 09:28:25 +0200

    feat : add applicaton repository

--- repository/src/main/java/be/portal/job/repositories/ApplicationRepository.java
+++ repository/src/main/java/be/portal/job/repositories/ApplicationRepository.java
@@ -3,5 +3,9 @@ package be.portal.job.repositories;
 import be.portal.job.entities.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+
 }
