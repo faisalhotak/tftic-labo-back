65c1810688799cd8dd1f49d4937af172ea0ca7a2
Lopidurs <nathan.sancke@hotmail.fr>
2024-05-23 11:21:02 +0200

    Test(experienceDetail): init db

---
+++ config/src/main/java/be/portal/job/utils/datainitializer/ExperienceDetailInit.java
@@ -0,0 +1,35 @@
+package be.portal.job.utils.datainitializer;
+
+import be.portal.job.entities.ExperienceDetail;
+import be.portal.job.entities.JobSeeker;
+import be.portal.job.repositories.ExperienceDetailRepository;
+import be.portal.job.repositories.JobSeekerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Order(11)
+public class ExperienceDetailInit implements CommandLineRunner {
+
+    private final ExperienceDetailRepository experienceDetailRepository;
+    private final JobSeekerRepository jobSeekerRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<JobSeeker> jobSeekers = jobSeekerRepository.findAll();
+        List<ExperienceDetail> experienceDetails = List.of(
+                new ExperienceDetail( "Société A", "Développement d'applications Java", LocalDate.parse("2020-01-01"), LocalDate.parse("2021-01-01"), jobSeekers.get(0)),
+                new ExperienceDetail( "Société B", "Développement d'applications JS", LocalDate.parse("2019-01-01"), LocalDate.parse("2020-01-01"), jobSeekers.get(0)),
+                new ExperienceDetail( "Société C", "Développement d'applications Python", LocalDate.parse("2018-01-01"), LocalDate.parse("2019-01-01"), jobSeekers.get(1))
+        );
+
+        experienceDetailRepository.saveAll(experienceDetails);
+    }
+}
---
+++ repository/src/main/java/be/portal/job/repositories/ExperienceDetailRepository.java
@@ -0,0 +1,7 @@
+package be.portal.job.repositories;
+
+import be.portal.job.entities.ExperienceDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExperienceDetailRepository extends JpaRepository<ExperienceDetail, Long>{
+}
