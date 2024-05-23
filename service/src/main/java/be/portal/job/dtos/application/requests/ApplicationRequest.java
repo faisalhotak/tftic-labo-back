d058a1d65febb9e270946bf6af046137cf69bfaf
Kei-gits <kioo-17@hotmail.com>
2024-05-23 09:28:15 +0200

    feat : add applicaton request

--- service/src/main/java/be/portal/job/dtos/application/requests/ApplicationRequest.java
+++ service/src/main/java/be/portal/job/dtos/application/requests/ApplicationRequest.java
@@ -1,25 +1,35 @@
 package be.portal.job.dtos.application.requests;
 
 import be.portal.job.entities.Application;
+import be.portal.job.entities.JobOffer;
+import be.portal.job.entities.JobSeeker;
 import be.portal.job.enums.ApplicationStatus;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record ApplicationRequest(
 
-        @NotBlank(message = "The date is required")
+        @NotNull(message = "The date is required")
         LocalDateTime apply_date,
 
-        @NotBlank(message = "The status is required")
-        ApplicationStatus applicationStatus
+        @NotNull(message = "The status is required")
+        ApplicationStatus applicationStatus,
+
+        @NotNull(message = "The id is required")
+        Long jobOfferId
+
 ) {
-    public Application toEntity() {
+    public Application toEntity(JobSeeker jobSeeker, JobOffer jobOffer) {
+
         Application application = new Application();
 
         application.setApplyDate(apply_date);
         application.setApplicationStatus(applicationStatus);
+        application.setJobOffer(jobOffer);
+        application.setJobSeeker(jobSeeker);
 
         return application;
+
     }
 }
