cc8cfd0a6adabb0f669d2b1d80d1510bcca8171c
Tomás Mendes <tomashm16@gmail.com>
2024-05-28 16:52:42 +0200

    refactor: changes in Contract Type

--- repository/src/main/java/be/portal/job/repositories/ContractTypeRepository.java
+++ repository/src/main/java/be/portal/job/repositories/ContractTypeRepository.java
@@ -2,9 +2,15 @@ package be.portal.job.repositories;
 
 import be.portal.job.entities.ContractType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ContractTypeRepository extends JpaRepository<ContractType, Long> {
+
+    @Query("SELECT ct FROM ContractType ct WHERE ct.name LIKE :name")
+    Optional<ContractType> findByName(String name);
 }
 
