f487629a28cb00f74cabfa1058a9c761de4179b4
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-05-28 17:32:25 +0200

    feat: added job function mapper, refactored the controllers and services, updated the DTOS

--- common/src/main/java/be/portal/job/dtos/job_function/requests/JobFunctionRequest.java
+++ common/src/main/java/be/portal/job/dtos/job_function/requests/JobFunctionRequest.java
@@ -1,15 +1,8 @@
 package be.portal.job.dtos.job_function.requests;
 
-import be.portal.job.entities.JobFunction;
 import jakarta.validation.constraints.NotBlank;
 
 public record JobFunctionRequest (
         @NotBlank(message="The job function name cannot be empty.")
-        String name,
-        @NotBlank(message="The job function title cannot be empty.")
-        String title
-) {
-    public JobFunction toEntity() {
-        return new JobFunctionRequest(this.name, this.title).toEntity();
-    }
-}
+        String name
+) { }
