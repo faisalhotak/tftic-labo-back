090d90b75620a2ceacd4275098baa3dde59cb56c
Tomás Mendes <tomashm16@gmail.com>
2024-05-29 19:54:16 +0200

    feat: add Skill set request dto

---
+++ common/src/main/java/be/portal/job/dtos/skill_set/requests/SkillSetRequest.java
@@ -0,0 +1,31 @@
+package be.portal.job.dtos.skill_set.requests;
+
+import be.portal.job.entities.JobSeeker;
+import be.portal.job.entities.SkillDetail;
+import be.portal.job.entities.SkillSet;
+import be.portal.job.enums.SkillLevel;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record SkillSetRequest(
+
+        @NotNull(message = "Skill level cannot be null")
+        SkillLevel skillLevel,
+
+        @Min(value = 0)
+        int years,
+
+        @NotNull(message = "Skill detail ID cannot be null")
+        Long skillDetailId
+) {
+    public SkillSet toEntity(JobSeeker jobSeeker, SkillDetail skillDetail) {
+        SkillSet skillSet = new SkillSet();
+
+        skillSet.setYears(years);
+        skillSet.setSkillLevel(skillLevel);
+        skillSet.setJobSeeker(jobSeeker);
+        skillSet.setSkillDetail(skillDetail);
+
+        return skillSet;
+    }
+}
