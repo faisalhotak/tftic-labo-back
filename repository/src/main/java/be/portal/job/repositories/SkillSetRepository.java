7556291e375ad223276bfc118613d02b916a5555
Tomás Mendes <tomashm16@gmail.com>
2024-05-29 19:53:56 +0200

    feat: add Skill set repository

---
+++ repository/src/main/java/be/portal/job/repositories/SkillSetRepository.java
@@ -0,0 +1,22 @@
+package be.portal.job.repositories;
+
+import be.portal.job.entities.SkillSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SkillSetRepository extends JpaRepository<SkillSet, Long> {
+
+    @Query("SELECT ss FROM SkillSet ss WHERE ss.jobSeeker.id = :seekerId")
+    List<SkillSet> findByJobSeekerId(Long seekerId);
+
+    @Query("SELECT ss FROM SkillSet ss WHERE ss.id = :id AND ss.jobSeeker.id = :seekerId")
+    Optional<SkillSet> findByIdAndJobSeekerId(Long id, Long seekerId);
+
+    @Query("SELECT ss FROM SkillSet ss WHERE ss.jobSeeker.id = :seekerId AND ss.skillDetail.id = :skillDetailId")
+    Optional<SkillSet> findByJobSeekerIdAndSkillDetailId(Long seekerId, Long skillDetailId);
+}
