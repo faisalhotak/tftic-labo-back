ffb1e3f486044e4c71d783d1d6b90f5f6c33eefe
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-05-29 21:14:20 +0200

    refactoring: updated response dto, service and repo

--- repository/src/main/java/be/portal/job/repositories/RoleRepository.java
+++ repository/src/main/java/be/portal/job/repositories/RoleRepository.java
@@ -3,7 +3,6 @@ package be.portal.job.repositories;
 import be.portal.job.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +11,5 @@ import java.util.Optional;
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     @Query("SELECT r FROM Role r WHERE r.name = :name")
-    Optional<Role> findByName(@Param("name") String name);
+    Optional<Role> findByName(String name);
 }
