39eba80ec8dc3deff2360ddb28dbb7fda321077e
Tomás Mendes <tomashm16@gmail.com>
2024-05-29 19:53:18 +0200

    feat: add Skill set services

---
+++ service/src/main/java/be/portal/job/services/ISkillSetService.java
@@ -0,0 +1,45 @@
+package be.portal.job.services;
+
+import be.portal.job.dtos.skill_set.requests.SkillSetRequest;
+import be.portal.job.dtos.skill_set.requests.SkillSetUpdateRequest;
+import be.portal.job.dtos.skill_set.responses.SkillSetResponse;
+
+import java.util.List;
+
+public interface ISkillSetService {
+
+    /**
+     * Retrieves all skill sets associated with the authenticated job seeker.
+     * @return a list of SkillSetResponse objects representing all skill sets of the authenticated job seeker.
+     */
+    List<SkillSetResponse> getAllBySeeker();
+
+    /**
+     * Retrieves a specific skill set by its ID.
+     * @param id the ID of the skill set to be retrieved.
+     * @return a SkillSetResponse object representing the skill set with the specified ID.
+     */
+    SkillSetResponse getById(Long id);
+
+    /**
+     * Adds a new skill set for the authenticated job seeker.
+     * @param request the SkillSetRequest object containing the details of the skill set to be added.
+     * @return a SkillSetResponse object representing the added skill set.
+     */
+    SkillSetResponse add(SkillSetRequest request);
+
+    /**
+     * Updates an existing skill set for the authenticated job seeker.
+     * @param id the ID of the skill set to be updated.
+     * @param request the SkillSetUpdateRequest object containing the new details of the skill set.
+     * @return a SkillSetResponse object representing the updated skill set.
+     */
+    SkillSetResponse update(Long id, SkillSetUpdateRequest request);
+
+    /**
+     * Deletes a specific skill set by its ID.
+     * @param id the ID of the skill set to be deleted.
+     * @return a SkillSetResponse object representing the deleted skill set.
+     */
+    SkillSetResponse delete(Long id);
+}
