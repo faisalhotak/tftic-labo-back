d183ee884b4c4516b6127498564daa24d5548368
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-05-29 21:30:22 +0200

    fix: removed useless CertificationDetailUpdateRequest and updated the controller and service accordingly

--- controller/src/main/java/be/portal/job/controllers/CertificationDetailController.java
+++ controller/src/main/java/be/portal/job/controllers/CertificationDetailController.java
@@ -1,8 +1,6 @@
 package be.portal.job.controllers;
 
-
 import be.portal.job.dtos.certification_detail.requests.CertificationDetailRequest;
-import be.portal.job.dtos.certification_detail.requests.CertificationDetailUpdateRequest;
 import be.portal.job.dtos.certification_detail.responses.CertificationDetailResponse;
 import be.portal.job.services.ICertificationDetailService;
 import jakarta.validation.Valid;
@@ -39,7 +37,7 @@ public class CertificationDetailController {
     @PutMapping("/{id:^[0-9]+$}")
     public ResponseEntity<CertificationDetailResponse> update(
             @PathVariable Long id,
-            @RequestBody @Valid CertificationDetailUpdateRequest request
+            @RequestBody @Valid CertificationDetailRequest request
     ) {
         return ResponseEntity.ok(certificationDetailService.update(id, request));
     }
