8b9483b897bb156e9c21b25c7f218bdd8cf4ce01
Tomás Mendes <tomashm16@gmail.com>
2024-05-29 19:52:21 +0200

    feat: add Skill Detail dto's

--- common/src/main/java/be/portal/job/dtos/skill_set/responses/SkillSetResponse.java
+++ common/src/main/java/be/portal/job/dtos/skill_set/responses/SkillSetResponse.java
@@ -1,26 +1,22 @@
 package be.portal.job.dtos.skill_set.responses;
 
+import be.portal.job.dtos.skill_detail.responses.SkillDetailResponse;
 import be.portal.job.entities.SkillSet;
 import be.portal.job.enums.SkillLevel;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 public record SkillSetResponse(
 
-        @NotNull(message = "Skill level cannot be null")
+        Long id,
         SkillLevel skillLevel,
-
-        @Min(value = 0)
         int years,
-
-        @NotNull
-        Long skillDetailId
+        SkillDetailResponse skillDetailId
 ) {
-    public SkillSetResponse fromEntity(SkillSet skillSet) {
+    public static SkillSetResponse fromEntity(SkillSet skillSet) {
         return new SkillSetResponse(
+                skillSet.getId(),
                 skillSet.getSkillLevel(),
                 skillSet.getYears(),
-                skillSet.getSkillDetail().getId()
+                SkillDetailResponse.fromEntity(skillSet.getSkillDetail())
         );
     }
 }
