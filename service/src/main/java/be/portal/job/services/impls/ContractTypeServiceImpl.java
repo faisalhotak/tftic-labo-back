112aef1da04d337ae01edd9c0a67aee181d1017e
Kei-gits <kioo-17@hotmail.com>
2024-05-30 14:52:58 +0200

    fix rename ContratTypeImpl in ContractTypeServiceImpl

--- service/src/main/java/be/portal/job/services/impls/ContractTypeServiceImpl.java
+++ service/src/main/java/be/portal/job/services/impls/ContractTypeServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ContractTypeImpl implements IContractTypeService {
+public class ContractTypeServiceImpl implements IContractTypeService {
 
     private final ContractTypeRepository contractTypeRepository;
 
