40c9139ff8596af09b57997995a546b2d3778cb3
Faisal HOTAK <55158944+faisalhotak@users.noreply.github.com>
2024-07-11 19:00:14 +0200

    fix: removed useless logging

--- service/src/main/java/be/portal/job/services/impls/JobOfferServiceImpl.java
+++ service/src/main/java/be/portal/job/services/impls/JobOfferServiceImpl.java
@@ -45,8 +45,6 @@ public class JobOfferServiceImpl implements IJobOfferService {
         Page<JobOffer> pagedJobOffers = jobOfferRepository
                 .findAll(JobOfferSpecifications.filterByParams(params), pageable);
 
-        System.out.println(pagedJobOffers);
-
         return new PagedJobOfferResponse(
                 pagedJobOffers.map(jobOfferMapper::fromEntity).getContent(),
                 pagedJobOffers.getSize(),
